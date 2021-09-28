fe8b1d4477a0c2f8896dab3571e046e622013a76
spadefaith <cedrickcampoto@yahoo.com>
2021-09-28 10:34:35 +0800

    c

--- global-client.js
+++ global-client.js
@@ -4,7 +4,7 @@
     global.cfg = {};
     if (type == 'production'){
         global.cfg.home = `https://lucky888bet.herokuapp.com`;
-        global.cfg.game = `http:3.133.4.140/`;
+        global.cfg.game = `http://3.133.4.140:5000`;
     } else if (type == 'development'){
         global.cfg.home = `http://localhost:7766`;
         global.cfg.game = `http://localhost:7778`;
